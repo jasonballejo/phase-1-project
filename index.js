f4494acc0a7a10c60521428373c69824ede7c37a
Jason Ballejo <ballejo19@gmail.com>
2022-09-21 22:57:00 -0400

    Removed old html data from JS

--- index.js
+++ index.js
@@ -71,36 +71,6 @@ function renderPokemon(e) {
         } 
       })
 
-
-        // document.querySelector('.flexbox-container').innerHTML = ` 
-    
-    // <div class="flexbox-item flexbox-item-1">
-    // <div class="column-one">
-    //     <h2>${capFirstLetter(data.name)}</h2> 
-    //     <img id="pokeImage" src="${data.sprites.other["home"].front_default}">
-    // </div>
-    
-    // <div class="column-two">
-    //     <h3>Pokemon Stats</h3>
-    //     <p>Number: ${data.id}</p>
-    //     <p>Height: ${data.height} feet</p>
-    //     <p>Weight: ${data.weight} lbs</p>
-    //     <p>Type: ${capFirstLetter(data.types[0].type.name)}</p>
-    //     <p>Base Stat: ${data.base_experience}</p>
-    //     <p>Best Move: ${capFirstLetter(data.moves[1].move.name)}</p>
-    // </div>
-
-    // <div class="column-three">
-    //     <p>HP: ${data.stats[0].base_stat}</p>                 
-    //     <p>Attack: ${data.stats[1].base_stat}</p>
-    //     <p>Defense: ${data.stats[2].base_stat}</p>
-    //     <p>Special Attack: ${data.stats[3].base_stat}</p>
-    //     <p>Special Defense: ${data.stats[4].base_stat}</p>
-    //     <p>Speed: ${data.stats[5].base_stat}</p>
-    //     <p>Ability: ${capFirstLetter(data.abilities[0].ability.name)}</p>
-    // </div>
-    // `
-
     // event listener mouse over/out on image   
     container.addEventListener('mouseover', () => {
         document.getElementById('pokeImage').src=`${imageShiny}`
